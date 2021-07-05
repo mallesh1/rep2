85ae695bb868eb5113fd45a59164bbf7f4c1da41
Your Name <you@example.com>
2021-07-05 21:14:58 +0530

    hi this is install pipeline

--- f.sh
+++ f.sh
@@ -1,4 +1,28 @@
-pwd
-whoami
-sudo apt update -s
-sudo apt install apache2 -s
+pipeline {
+    stages {
+        stage('Terraform test') {
+            environment {
+                AWS_DEFAULT_REGION="us-east-1"
+            }
+            steps {
+                sh "AWS_DEFAULT_REGION=us-east-1 cd src && terraform init && terraform plan"
+            }
+        }
+        stage('Terraform Apply us-east-1') {
+            environment {
+                AWS_DEFAULT_REGION="us-east-1"
+            }
+            steps {
+                sh "cd src && terraform init && terraform apply --auto-approve"
+            }
+        }
+        stage('Terraform Apply eu-west-1') {
+            environment {
+                AWS_DEFAULT_REGION="us-east-1"
+            }
+            steps {
+                sh "AWS_DEFAULT_REGION=eu-west-1 cd src && terraform init && terraform apply --auto-approve"
+            }
+        }
+    }
+}
