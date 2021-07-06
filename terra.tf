69ad86640b3a7ecabf8b72e96fae22e820c10d5a
Your Name <you@example.com>
2021-07-06 13:48:14 +0530

    testing terraform

--- terra.tf
+++ terra.tf
@@ -26,3 +26,38 @@ resource aws_subnet "sbn" {
     name = "sbn"
   }
 }
+resource aws_internet_gateway "igw" {
+  vpc_id = aws_vpc.vpc-1.id
+  tags = {
+    name = "igw"
+  }
+}
+resource aws_route_table "rout" {
+  vpc_id = aws_vpc.vpc-1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource aws_route_table_association "routatach" {
+  count = 3
+  subnet_id      = aws_subnet.sbn.*.id[count.index]
+  route_table_id = aws_route_table.rout.id
+}
+
+resource aws_security_group "eks-sg" {
+  name        = "eks-sg"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.vpc-1.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-sg"
+  }
+}
